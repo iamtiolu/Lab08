2fd59da0e18bc815f157b57694fec8128383d9b8
Tiolu Olu-olayioye <tiolu@Tiolus-MacBook-Pro.local>
2026-03-03 20:41:56 -0700

    delete city Red phase

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -11,7 +11,16 @@ public class CustomListTest {
         City calgary = new City("Calgary", "AB");
         list.addCity(calgary);
 
-        // This will fail initially because hasCity() doesn't exist
+        assertTrue(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        list.deleteCity(calgary);
+
         assertTrue(list.hasCity(calgary));
     }
 }
