feb0c349db87c4178f3ce43c40e5b64d9a324fa5
Tiolu Olu-olayioye <tiolu@Tiolus-MacBook-Pro.local>
2026-03-03 21:02:42 -0700

    Count Cities Red Phase

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -23,4 +23,16 @@ public class CustomListTest {
 
         assertFalse(list.hasCity(calgary));
     }
+
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+
+        list.addCity(new City("Calgary", "AB"));
+        list.addCity(new City("Edmonton", "AB"));
+        list.addCity(new City("Toronto", "ON"));
+
+        assertEquals(3, list.countCities());
+    }
+
 }
